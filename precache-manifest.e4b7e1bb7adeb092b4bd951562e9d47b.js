2441cc1b6f109d23ed38a9fb6007cfd57e4d672a
21man2 <rachelelimman@gmail.com>
2020-05-29 20:17:41 -0400

    Updates

---
+++ precache-manifest.e4b7e1bb7adeb092b4bd951562e9d47b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7ee55f25dbd83d341e6b62bc3a2a12e",
+    "url": "/mag-filter/index.html"
+  },
+  {
+    "revision": "e29803e81b79ccc19f18",
+    "url": "/mag-filter/static/css/main.8718d147.chunk.css"
+  },
+  {
+    "revision": "d90935ebfd3ac94b7fdd",
+    "url": "/mag-filter/static/js/2.b02f6f87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mag-filter/static/js/2.b02f6f87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e29803e81b79ccc19f18",
+    "url": "/mag-filter/static/js/main.05d59259.chunk.js"
+  },
+  {
+    "revision": "02e57efeab3837be5eaa",
+    "url": "/mag-filter/static/js/runtime-main.151bdf63.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mag-filter/static/media/logo.5d5d9eef.svg"
+  }
+]);
